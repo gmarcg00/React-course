09eb31dfb2cde2e97d3db68658f361ea48227756
Guille <gmarcg00@estudiantes.unileon.es>
2023-02-18 17:50:24 +0100

    first react project

---
+++ react-first-project/src/components/Testimonio.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../hojas-de-estilo/Testimonio.css"
+
+function Testimonio(props){
+  return(
+    <div className="contenedor-testimonio">
+      <img className="imagen-testimonio" src={require(`../images/testimonio-${props.imagen}.png`)} alt="Foto de Emma"></img>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre} en {props.pais}</strong>
+        </p>
+        <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className="texto-testimonio">{props.testimonio}</p>
+      </div>
+    </div>
+  );    
+}
+
+export default Testimonio;
